5f9715326cf962f6cc5b094c478abca0d5a5f4c4
Patrick Martin <patrickwaltermartin@gmail.com>
2024-07-25 16:43:11 -0300

    Set up go lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     'williamboman/mason-lspconfig.nvim',
     config = function()
       require('mason-lspconfig').setup {
-        ensure_installed = { 'lua_ls', 'tsserver', 'pylsp', 'emmet_ls', 'cssls', 'tailwindcss' },
+        ensure_installed = { 'lua_ls', 'gopls', 'tsserver', 'pylsp', 'emmet_ls', 'cssls', 'tailwindcss' },
       }
     end,
   },
@@ -24,6 +24,7 @@ return {
       lspconfig.emmet_ls.setup { capabilities = capabilities }
       lspconfig.cssls.setup { capabilities = capabilities }
       lspconfig.tailwindcss.setup { capabilities = capabilities }
+      lspconfig.gopls.setup { capabilities = capabilities }
       vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
 --telescope keymaps
       vim.keymap.set('n', '<leader>sf', builtin.find_files,{desc='[S]earch [F]iles'})
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, {desc='[S]earch by [G]rep'})
+      vim.keymap.set('n', '<leader>sw', builtin.grep_string, {desc='[S]earch for [W]ord'})
      end
   },
   {
